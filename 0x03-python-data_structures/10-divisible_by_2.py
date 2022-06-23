f3b496774c1dfc177ddb0b7c7d4ff8846a18d9e0
AERENE <mugaadhiambo19@students.ku.ac.ke>
2022-06-22 22:38:00 -0700

    Update!

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,4 +6,5 @@ def divisible_by_2(my_list=[]):
     result = []
     for value in my_list:
         result.append(True) if value % 2 == 0 else result.append(False)
-    return result 
+    return result
+
